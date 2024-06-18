3ccea18936b9c6aae9879698a1088e6039b3d7ba
alewhite11 <alebix00@gmail.com>
2024-06-18 10:58:37 +0200

    publication setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import './App.css';
-import { TonConnectButton } from '@tonconnect/ui-react';
 
 function App() {
   const [score, setScore] = useState(0);
@@ -12,7 +11,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <TonConnectButton />
         <h1>Clicking Game</h1>
         <button onClick={handleClick}>Click me!</button>
         <p>Score: <span>{score}</span></p>
