6af9377e9a28d817b89716f0fe2a989e44021bf3
alewhite11 <alebix00@gmail.com>
2024-07-30 12:29:16 +0200

    general fixes

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-//import './index.css'
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 
-const manifestUrl = 'https://alewhite11.github.io/cryptoBot/tonconnect-manifest.json';
-
+const manifestUrl = 'https://planttoken-7e33c.web.app/tonconnect-manifest.json';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
