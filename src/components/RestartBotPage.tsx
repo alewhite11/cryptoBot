b67b50a320d0e0809caaf57bb79dd6b4f1d6a2c8
alewhite11 <alebix00@gmail.com>
2024-08-22 00:56:06 +0200

    fix: text on firebase branch

--- src/components/RestartBotPage.tsx
+++ src/components/RestartBotPage.tsx
@@ -5,7 +5,7 @@ const RestartBotPage = () => {
         <div className="Desktop-component">
           <h1 className="Desktop-title">PLANT</h1>
           <div className="Desktop-content">
-            <p style={{fontSize: 'large'}}>We updated the miniapp, to see the changes please turn back and type /start in the chat to upgread to the new version of the app 🌱. Your progresses will not be lost</p>
+            <p style={{fontSize: 'large'}}>We updated the miniapp, to see the changes please turn back and type /start in the chat to upgrade to the new version of the app 🌱. Your progresses will not be lost</p>
           </div>
         </div>
     );
